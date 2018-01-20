5cce796214841c9c9f6b270e115e2e3cf4dda647
Sakata Yoga <yogacahya08@gmail.com>
2018-01-20 09:32:09 +0700

    Second Commit Melengkapi kode

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl338/hitungsuhu/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl338/hitungsuhu/MainActivity.java
@@ -3,15 +3,132 @@ package id.sch.smktelkom_mlg.tugas01.xirpl338.hitungsuhu;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     int suhuawal = 0;
-
+    double suhuakhir = 0;
+    EditText sawal;
+    EditText sakhir;
+    Spinner aw, ak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sawal = findViewById(R.id.SuhuAwal);
+        sakhir = findViewById(R.id.SuhuAkhir);
+        aw = findViewById(R.id.awal);
+        ak = findViewById(R.id.akhir);
+        aw.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (aw.getSelectedItemPosition() == 0) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = suhuawal;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = suhuawal * 4 / 5;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal * 9 / 5 + 32;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal + 273;
+                    }
+                } else if (aw.getSelectedItemPosition() == 1) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = suhuawal * 5 / 4;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = suhuawal;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = (suhuawal * 5 / 4) * 9 / 5 + 32;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal * 5 / 4 + 273;
+                    }
+                } else if (aw.getSelectedItemPosition() == 2) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = (suhuawal - 32) * 5 / 9;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = ((suhuawal - 32) * 5 / 9) * 4 / 5;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal;
+                    } else if (ak.getSelectedItemPosition() == 3) {
+                        suhuakhir = ((suhuawal - 32) * 5 / 9) + 273;
+                    }
+                } else if (aw.getSelectedItemPosition() == 3) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = suhuawal - 273;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = (suhuawal - 273) * 4 / 5;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = (suhuawal - 273) * 9 / 5 + 32;
+                    } else if (ak.getSelectedItemPosition() == 3) {
+                        suhuakhir = suhuawal;
+                    }
+                }
+                sakhir.setText(String.valueOf(suhuakhir));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
+        ak.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (aw.getSelectedItemPosition() == 0) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = suhuawal;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = suhuawal * 4 / 5;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal * 9 / 5 + 32;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal + 273;
+                    }
+                } else if (aw.getSelectedItemPosition() == 1) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = suhuawal * 5 / 4;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = suhuawal;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = (suhuawal * 5 / 4) * 9 / 5 + 32;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal * 5 / 4 + 273;
+                    }
+                } else if (aw.getSelectedItemPosition() == 2) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = (suhuawal - 32) * 5 / 9;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = ((suhuawal - 32) * 5 / 9) * 4 / 5;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = suhuawal;
+                    } else if (ak.getSelectedItemPosition() == 3) {
+                        suhuakhir = ((suhuawal - 32) * 5 / 9) + 273;
+                    }
+                } else if (aw.getSelectedItemPosition() == 3) {
+                    if (ak.getSelectedItemPosition() == 0) {
+                        suhuakhir = suhuawal - 273;
+                    } else if (ak.getSelectedItemPosition() == 1) {
+                        suhuakhir = (suhuawal - 273) * 4 / 5;
+                    } else if (ak.getSelectedItemPosition() == 2) {
+                        suhuakhir = (suhuawal - 273) * 9 / 5 + 32;
+                    } else if (ak.getSelectedItemPosition() == 3) {
+                        suhuakhir = suhuawal;
+                    }
+                }
+                sakhir.setText(String.valueOf(suhuakhir));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
     }
 
     public void Klik(View v) {
@@ -40,10 +157,89 @@ public class MainActivity extends AppCompatActivity {
                 suhuawal *= 10;
                 suhuawal += 5;
                 break;
+
+            case R.id.angka6:
+                suhuawal *= 10;
+                suhuawal += 6;
+                break;
+
+            case R.id.angka7:
+                suhuawal *= 10;
+                suhuawal += 7;
+                break;
+
+            case R.id.angka8:
+                suhuawal *= 10;
+                suhuawal += 5;
+                break;
+
+            case R.id.angka9:
+                suhuawal *= 10;
+                suhuawal += 5;
+                break;
+
+            case R.id.angka0:
+                suhuawal *= 10;
+                suhuawal += 0;
+                break;
+
+            case R.id.clear:
+                suhuawal = 0;
+                sawal.setText("");
+                break;
+
+            case R.id.delete:
+                suhuawal /= 10;
+                sawal.setText(String.valueOf(suhuawal));
+                break;
         }
-        EditText awal = findViewById(R.id.SuhuAwal);
-        if (awal.getText().length() < 5) {
-            awal.setText(String.valueOf(suhuawal));
+
+        if (sawal.getText().length() < 5) {
+            sawal.setText(String.valueOf(suhuawal));
         }
+
+        if (aw.getSelectedItemPosition() == 0) {
+            if (ak.getSelectedItemPosition() == 0) {
+                suhuakhir = suhuawal;
+            } else if (ak.getSelectedItemPosition() == 1) {
+                suhuakhir = suhuawal * 4 / 5;
+            } else if (ak.getSelectedItemPosition() == 2) {
+                suhuakhir = suhuawal * 9 / 5 + 32;
+            } else if (ak.getSelectedItemPosition() == 2) {
+                suhuakhir = suhuawal + 273;
+            }
+        } else if (aw.getSelectedItemPosition() == 1) {
+            if (ak.getSelectedItemPosition() == 0) {
+                suhuakhir = suhuawal * 5 / 4;
+            } else if (ak.getSelectedItemPosition() == 1) {
+                suhuakhir = suhuawal;
+            } else if (ak.getSelectedItemPosition() == 2) {
+                suhuakhir = (suhuawal * 5 / 4) * 9 / 5 + 32;
+            } else if (ak.getSelectedItemPosition() == 2) {
+                suhuakhir = suhuawal * 5 / 4 + 273;
+            }
+        } else if (aw.getSelectedItemPosition() == 2) {
+            if (ak.getSelectedItemPosition() == 0) {
+                suhuakhir = (suhuawal - 32) * 5 / 9;
+            } else if (ak.getSelectedItemPosition() == 1) {
+                suhuakhir = ((suhuawal - 32) * 5 / 9) * 4 / 5;
+            } else if (ak.getSelectedItemPosition() == 2) {
+                suhuakhir = suhuawal;
+            } else if (ak.getSelectedItemPosition() == 3) {
+                suhuakhir = ((suhuawal - 32) * 5 / 9) + 273;
+            }
+        } else if (aw.getSelectedItemPosition() == 3) {
+            if (ak.getSelectedItemPosition() == 0) {
+                suhuakhir = suhuawal - 273;
+            } else if (ak.getSelectedItemPosition() == 1) {
+                suhuakhir = (suhuawal - 273) * 4 / 5;
+            } else if (ak.getSelectedItemPosition() == 2) {
+                suhuakhir = (suhuawal - 273) * 9 / 5 + 32;
+            } else if (ak.getSelectedItemPosition() == 3) {
+                suhuakhir = suhuawal;
+            }
+        }
+        sakhir.setText(String.valueOf(suhuakhir));
     }
+
 }
